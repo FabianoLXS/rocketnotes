bb59da6c5d49a69f52e752d5fe016475eccd5ef7
FabianoLXS <fab_luiz@msn.com>
2023-08-07 17:57:20 -0300

    commit

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,5 @@
+import { Container } from "./styles"
+
+export function Button(props) {
+  return <Container type="button">{props.title}</Container>
+};
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,15 @@
-import {Container} from "./styles"
+import { Container } from "./styles"
+import { Button } from "../../components/Button"
 
-export function Details(){
- 
+export function Details() {
   return (
     <Container>
       <h1>Hello World!</h1>
       <span>Fabiano Santos</span>
+
+      <Button title="Entrar" />
+      <Button title="Cadastrar" />
+      <Button title="Voltar" />
     </Container>
   )
 }
