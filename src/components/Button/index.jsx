5084848ae9063c885e52ed22cafd2db83210500d
FabianoLXS <fab_luiz@msn.com>
2023-08-07 17:58:05 -0300

    commit

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,5 +1,6 @@
 import { Container } from "./styles"
 
 export function Button(props) {
+  // eslint-disable-next-line react/prop-types
   return <Container type="button">{props.title}</Container>
 };
