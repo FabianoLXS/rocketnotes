3c5766c703b8a24cd7bdace2ef62ea3e317d53ad
Fabiano Luiz Santos <fab_luiz@msn.com>
2023-08-09 21:18:17 -0300

    commit

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,13 +1,9 @@
-import { Container } from "./styles"
+import { Container } from './styles'
 
-export function Button({title, loading = false, ...rest}) {
-  // eslint-disable-next-line react/prop-types
-  return <Container 
-  type="button"
-  disabled={loading}
-  {...rest}
-  >
-    {loading ? 'Carregando...' : title}
-  
-  </Container>
-};
+export function Button({ title, loading = false, ...rest }) {
+  return (
+    <Container type="button" disabled={loading} {...rest}>
+      {loading ? "Carregando..." : title}
+    </Container>
+  )
+}
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,14 +1,14 @@
 import { Container } from "./styles"
+import { Header } from "../../components/Button"
 import { Button } from "../../components/Button"
 
 export function Details() {
   return (
     <Container>
+      <Header />
       <h1>Hello World!</h1>
       <span>Fabiano Santos</span>
 
-      <Button title="Login" loading />
-      <Button title="Cadastrar" />
       <Button title="Voltar" />
     </Container>
   )
