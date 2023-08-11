cd6e36ee80458abffa4bad1ed8e8b7b009236fac
Fabiano Luiz Santos <fab_luiz@msn.com>
2023-08-11 15:09:47 -0300

    commit

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,13 @@ import {Container} from './styles'
 export function Header(){
   return(
     <Container>
-
+      <Profile>
+        <img src="https://github.com/fabianolxs.png" alt="" />
+        <div>
+          <span>Bem-vindo</span>
+          <strong>Fabiano Xavier</strong>
+          </div>
+      </Profile>
     </Container>
   )
 }
